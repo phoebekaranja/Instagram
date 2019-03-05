04fcc6bffa042b6e1f29998bb66814fc462a709e
phoebekaranja <phoebemurish@gmail.com>
2019-03-05 16:24:22 +0300

    created an url pattern and made a function for the searchresults

--- epic/urls.py
+++ epic/urls.py
@@ -3,4 +3,5 @@ from .import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^$',views.image_today,name='image_Today'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
--- epic/views.py
+++ epic/views.py
@@ -14,3 +14,17 @@ def convert_dates(dates):
     '''
     day = days[day_number]
     return day
+def image_today(request):
+    epic = Image.gallery_images()
+    category= Categorys.objects.all()
+    location= Location.objects.all()
+    return render(request,'all-images/gallery-images.html',{"epic":epic,'category':category,"location":location})
+def search_results(request):
+    category= Categorys.objects.all()
+    location= Location.objects.all()
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_image,'category':category,"location":location})    
