151ddb3a4d5551e7ddb0c1fdfcd0ecc220c832c2
phoebekaranja <phoebemurish@gmail.com>
2019-03-05 16:08:59 +0300

    adding images inthe url files

--- epic/urls.py
+++ epic/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from .import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.image_today,name='image_Today'),
 ]
--- epic/views.py
+++ epic/views.py
@@ -1,5 +1,5 @@
 from django.http  import HttpResponse
-
+import datetime as dt
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
