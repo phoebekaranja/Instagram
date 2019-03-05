f8c640ce98b403d9a6ff933789896fc147f8f35c
phoebekaranja <phoebemurish@gmail.com>
2019-03-05 16:12:17 +0300

    maadding function in views py file for convert dates

--- epic/views.py
+++ epic/views.py
@@ -3,3 +3,14 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+def convert_dates(dates):
+    '''
+    function that gets the weekday number of the date.
+    '''
+    day_number = dt.date.weekday(dates)
+    days =['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    '''
+    returning the actual day of the week
+    '''
+    day = days[day_number]
+    return day
