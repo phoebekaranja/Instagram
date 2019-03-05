5be1668178bc701195f696f165ff571d6e1b6b82
phoebekaranja <phoebemurish@gmail.com>
2019-03-05 18:16:49 +0300

    added an url pattern

--- epic/urls.py
+++ epic/urls.py
@@ -4,4 +4,7 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^$',views.image_today,name='image_Today'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^images/(\d+)',views.Image,name ='image'),
+    url(r'^category/(\w+)',views.category_image,name='category_list'),
+    url(r'^location/(\w+)',views.location_image,name='location_list'),
 ]
