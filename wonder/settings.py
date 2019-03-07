24bb2910a5b18dc565f006e7d040f054ee1fdda8
phoebekaranja <phoebemurish@gmail.com>
2019-03-07 12:56:19 +0300

    making changes

--- wonder/settings.py
+++ wonder/settings.py
@@ -161,6 +161,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
